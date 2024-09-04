03e4c2f7069cc2fe5a33b2ed33a728b2aa4baed3
muaymi <muaymiimuay@gmail.com>
2024-09-04 22:24:36 +0700

    feat: create form to register and save to database

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user, notice: 'User was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :birthday, :gender, :email, :phone_number, :subject)
+  end
+end
