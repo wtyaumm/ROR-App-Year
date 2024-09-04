4ef04804449714cc4830ccd13b49225d5118eea9
muaymi <muaymiimuay@gmail.com>
2024-09-04 22:57:06 +0700

    feat: add edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,22 @@ class UsersController < ApplicationController
     end
   end
 
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :birthday, :gender, :email, :phone_number, :subject)
+  def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :birthday, :gender, :email, :phone_number, :subject)
+    end
 end
